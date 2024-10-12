da3ae1c64432bd82801fee0c367b1429da5a666a
Zhangxinhua0 <2982754849@qq.com>
2024-10-12 22:03:46 +0800

    实验2

--- Solution4.java
+++ Solution4.java
@@ -30,34 +30,40 @@ import java.util.Arrays;
 class Solution4 {
     public int maximumGap(int[] nums) {
 
-        int n = nums.length - 1;
+        int n = nums.length;
+        int ret = 0;
+
+        //数组元素个数小于 2，因此返回 0。
         if (n < 2) {
             return 0;
         }
-        long exp = 1;
-        int[] buf = new int[n];
-        int maxVal = Arrays.stream(nums).max().getAsInt();
-
-        while (maxVal > exp) {
-            int[] cnt = new int[10];
-            for (int i = 0; i < n; i++) {
-                int digit = (nums[i] / (int) exp) % 10;
-                cnt[digit]++;
+        // long exp = 1;
+        // int[] buf = new int[n];
+        // //找出数组中的最大值
+        // int maxVal = Arrays.stream(nums).max().getAsInt();
+        
+        // while (maxVal > exp) {
+        //     //给数组排序
+        //     int[] cnt = new int[10];
+        //     for (int i = 0; i < n; i++) {
+        //         int digit = (nums[i] / (int) exp) % 10;
+        //         cnt[digit]++;
+        //     }
+        //     for (int i = 1; i < 10; i++){
+        //         cnt[i] += cnt[i - 1];
+        //     for (int j = n - 1; j >= 0; j--) {
+        //         int digit = (nums[j] / (int) exp) % 10;
+        //         buf[cnt[digit] - 1] = nums[j];
+        //         cnt[digit]--;
+        //     }
+        //     System.arraycopy(buf, 0, nums, 0, n);
+        //     exp += 10;
+        //     }
+        // }
+        //遍历数组，比较相邻两数间的差值
+                for (int i = 1; i < n; i++) {
+                ret = Math.max(ret, nums[i] - nums[i - 1]);
             }
-            for (int i = 1; i < 10; i++){
-                cnt[i] += cnt[i - 1];
-            for (int i = n - 1; i >= 0; i--) {
-                int digit = (nums[i] / (int) exp) % 10;
-                buf[cnt[digit] - 1] = nums[i];
-                cnt[digit]--;
-            }
-            System.arraycopy(buf, 0, nums, 0, n);
-            exp += 10;
-        }
-
-        int ret = 0;
-            for (int i = 1; i < n; i++) {
-            ret = Math.max(ret, nums[i] - nums[i - 1]);
-        }return ret;
+        return ret;
     }
 }
